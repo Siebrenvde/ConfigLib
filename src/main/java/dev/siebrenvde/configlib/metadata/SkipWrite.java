88fb88690bbf26620c1b266c2b6f3c82a36bfcb0
Siebrenvde <siebren@siebrenvde.dev>
2025-03-09 18:24:13 +0100

    Add SkipWrite#skipWhen() methods

--- src/main/java/dev/siebrenvde/configlib/metadata/SkipWrite.java
+++ src/main/java/dev/siebrenvde/configlib/metadata/SkipWrite.java
@@ -1,6 +1,10 @@
 package dev.siebrenvde.configlib.metadata;
 
 import org.quiltmc.config.api.metadata.MetadataType;
+import org.quiltmc.config.impl.ConfigFieldAnnotationProcessors;
+
+import java.lang.annotation.Annotation;
+import java.util.function.Supplier;
 
 /**
  * When added to a node's metadata, prevents the node
@@ -11,6 +15,31 @@ import org.quiltmc.config.api.metadata.MetadataType;
 public class SkipWrite {
 
     public static MetadataType<SkipWrite, Builder> TYPE = MetadataType.create(Builder::new);
+
+    /**
+     * Registers the provided annotation to <b><u>not</u></b> write its affected field when the condition is <code>true</code>
+     * @param annotationClass the annotation to register
+     * @param condition the condition
+     */
+    @SuppressWarnings("unused")
+    public static void skipWhen(Class<? extends Annotation> annotationClass, boolean condition) {
+        skipWhen(annotationClass, () -> condition);
+    }
+
+    /**
+     * Registers the provided annotation to <b><u>not</u></b> write its affected field when the condition is <code>true</code>
+     * @param annotationClass the annotation to register
+     * @param condition the condition
+     */
+    @SuppressWarnings({"ResultOfMethodCallIgnored", "unused"})
+    public static void skipWhen(Class<? extends Annotation> annotationClass, Supplier<Boolean> condition) {
+        ConfigFieldAnnotationProcessors.register(
+            annotationClass,
+            (annotation, builder) -> {
+                if (condition.get()) builder.metadata(SkipWrite.TYPE, Builder::build);
+            }
+        );
+    }
 
     public static final class Builder implements MetadataType.Builder<SkipWrite> {
         @SuppressWarnings("InstantiationOfUtilityClass")
